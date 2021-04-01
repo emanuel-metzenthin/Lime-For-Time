95fc19ca5588456f2c4eec490b41f0da5d6cb363
Cesare <cesare@ctrl-z-bg.org>
2021-04-01 18:49:39 +0200

    requirements.txt added to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
 
+INSTALL_REQUIRES = open("requirements.txt").readlines()
+
 setup(
     name='lime_timeseries',
     version='0.1',
+    install_requires = INSTALL_REQUIRES,
     description='Applying the LIME algorithm for explainable machine learning to time series data',
     url='https://github.com/emanuel-metzenthin/Lime-For-Time',
     py_modules=[
